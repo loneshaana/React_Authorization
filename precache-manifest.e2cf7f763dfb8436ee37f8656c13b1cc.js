7163fcdf8030450f752962a80c735469e0483843
Anwar <anwar@milvik.se>
2019-05-19 20:06:53 +0530

    Updates

---
+++ precache-manifest.e2cf7f763dfb8436ee37f8656c13b1cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0468038a53279955ec01d970a1fadcae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bd3b88860667a0c0fe",
+    "url": "/static/css/main.f2ddf532.chunk.css"
+  },
+  {
+    "revision": "497b86d617e77b9e0794",
+    "url": "/static/js/2.9b33f384.chunk.js"
+  },
+  {
+    "revision": "49bd3b88860667a0c0fe",
+    "url": "/static/js/main.7a205e3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
